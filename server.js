37ff25352626d282d5f1183953a70d3b22ab8b9d
llorenc <llollo6613@gmail.com>
2026-04-16 14:43:36 +0200

    fix port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors    = require('cors');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const HOSTEX_TOKEN = process.env.HOSTEX_TOKEN || '';
 const HOSTEX_BASE  = 'https://api.hostex.io/v3';
