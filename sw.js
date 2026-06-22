7efabda397005b86aebb231fdd57e73a0232ec4f
llorenc <llollo6613@gmail.com>
2026-06-22 22:33:48 +0200

    revenus: commission reelle Hostex par resa (fin des taux fixes)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'illiberis-v3';
+const CACHE = 'illiberis-v4';
 const OFFLINE_URLS = ['/menage', '/mobile', '/manifest-menage.json', '/icon-menage-192.png', '/icon-menage-512.png'];
 
 self.addEventListener('install', e => {
